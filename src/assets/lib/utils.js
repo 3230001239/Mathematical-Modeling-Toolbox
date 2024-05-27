e986a062ab9bd776346f02bfb9cb3b0bd9ea30e9
3230001239 <3230001239@qq.com>
2024-05-27 23:28:57 +0800

    Delete src directory

--- src/assets/lib/utils.js
+++
@@ -1,9 +0,0 @@
-export function readFile(file){
-	return new Promise(resolve =>{
-		let reader = new FileReader();
-		reader.readAsArrayBuffer(file);
-		reader.onload = ev =>{
-			resolve(ev.target.result);
-		};
-	});
-}
